48fe29ca31009e79a3c4749a6404d84adb0f6311
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-05-24 12:09:39 +0000

    good to go
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/sem-fancy-excerpt/trunk@1490 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- sem-fancy-excerpt.php
+++ sem-fancy-excerpt.php
@@ -3,7 +3,7 @@
 Plugin Name: Fancy Excerpt
 Plugin URI: http://www.semiologic.com/software/fancy-excerpt/
 Description: Enhances WordPress' default excerpt generator by generating paragraph aware excerpts.
-Version: 3.0 beta
+Version: 3.0 RC
 Author: Denis de Bernardy
 Author URI: http://www.getsemiologic.com
 Text Domain: sem-fancy-excerpt-info
@@ -20,69 +20,61 @@ http://www.opensource.org/licenses/gpl-2.0.php
 **/
 
 
+remove_filter('get_the_excerpt', 'wp_trim_excerpt');
+add_filter('get_the_excerpt', 'fancy_excerpt', 0);
+
 /**
- * fancy_excerpt
+ * fancy_excerpt()
  *
- * @package Fancy Excerpt
+ * @param string $content
+ * @return string $content
  **/
 
-remove_filter('get_the_excerpt', 'wp_trim_excerpt');
-add_filter('get_the_excerpt', array('fancy_excerpt', 'process'), 0);
-
-class fancy_excerpt {
-	/**
-	 * process()
-	 *
-	 * @param string $content
-	 * @return string $content
-	 **/
-
-	function process($text) {
-		$text = trim($text);
+function fancy_excerpt($text) {
+	$text = trim($text);
+	
+	if ( $text || !in_the_loop() )
+		return wp_trim_excerpt($text);
+	
+	global $allowedposttags;
+	
+	$more = __('More...');
+	
+	$text = get_the_content($more);
+	$text = strip_shortcodes($text);
+	$text = str_replace(array("\r\n", "\r"), "\n", $text);
+	$text = preg_replace("/<(script|style|object)(?:\s[^<>]*)?>.*?<\/\\1>/is", '', $text);
+	$text = wp_kses($text, $allowedposttags);
+	
+	if ( !preg_match("|$more</a>$|", $text)
+		&& count(preg_split("~\s+~", trim(strip_tags($text)))) > 30
+	) {
+		# automatically add a more... tag
 		
-		if ( $text || !in_the_loop() )
-			return wp_trim_excerpt($text);
+		$bits = preg_split("~(<(?:h[1-6]|p|ul|ol|li|dl|dd|table|tr|pre|blockquote)\b[^>]*>|\n+)~i", $text, null, PREG_SPLIT_DELIM_CAPTURE);
+		$text = '';
+		$length = 0;
 		
-		global $allowedposttags;
-		
-		$more = __('More...');
-		
-		$text = get_the_content($more);
-		$text = strip_shortcodes($text);
-		$text = str_replace(array("\r\n", "\r"), "\n", $text);
-		$text = preg_replace("/<(script|style|object)(?:\s[^<>]*)?>.*?<\/\\1>/is", '', $text);
-		$text = wp_kses($text, $allowedposttags);
-		
-		if ( !preg_match("|$more</a>$|", $text)
-			&& count(preg_split("~\s+~", trim(strip_tags($text)))) > 30
-		) {
-			# automatically add a more... tag
+		foreach ( $bits as $bit ) {
+			$text .= $bit;
+			$count += count(preg_split("~\s+~", trim(strip_tags($bit))));
 			
-			$bits = preg_split("~(<(?:h[1-6]|p|ul|ol|li|dl|dd|table|tr|pre|blockquote)\b[^>]*>|\n+)~i", $text, null, PREG_SPLIT_DELIM_CAPTURE);
-			$text = '';
-			$length = 0;
-			
-			foreach ( $bits as $bit ) {
-				$text .= $bit;
-				$count += count(preg_split("~\s+~", trim(strip_tags($bit))));
-				
-				if ( $count > 30 )
-					break;
-			}
-			
-			$text = force_balance_tags($text);
-			
-			$text .= "\n\n"
-				. '<p>'
-				. ' <a href="'. esc_url(get_permalink()) . '" class="more-link">'
-					. $more
-					. '</a>'
-				. '</p>' . "\n";
-			
-			$text = apply_filters('the_content', $text);
+			if ( $count > 30 )
+				break;
 		}
 		
-		return apply_filters('wp_trim_excerpt', $text, '');
-	} # process()
-} # fancy_excerpt
+		$text = force_balance_tags($text);
+		
+		$text .= "\n\n"
+			. '<p>'
+			. ' <a href="'. esc_url(get_permalink()) . '" class="more-link">'
+				. $more
+				. '</a>'
+			. '</p>' . "\n";
+		
+		$text = apply_filters('the_content', $text);
+	}
+	
+	return apply_filters('wp_trim_excerpt', $text, '');
+} # fancy_excerpt()
 ?>
